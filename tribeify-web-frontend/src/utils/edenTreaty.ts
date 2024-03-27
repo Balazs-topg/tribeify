7979a3e67b96de63f5ce1b8aa56fe0966e23fa94
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2024-03-27 18:37:46 +0100

    Edit ts config

--- tribeify-web-frontend/src/utils/edenTreaty.ts
+++ tribeify-web-frontend/src/utils/edenTreaty.ts
@@ -1,5 +1,4 @@
 import type { App } from "../../../tribeify-backend/app/src/index";
 import { treaty } from "@elysiajs/eden";
 
-//@ts-ignore
 export const backendApi = treaty<App>("localhost:8080");
