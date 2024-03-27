b27cc4478c9c5aeb6157ee356761e19b990953f2
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2024-03-27 17:39:06 +0100

    Actually fix build error

--- tribeify-web-frontend/src/app/layout.tsx
+++ tribeify-web-frontend/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import Navigation from "./Navigation";
 import "./globals.css";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
