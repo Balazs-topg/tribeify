0774b6fd090f2009b90e8a75b58a5855a2564138
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2024-03-27 18:33:37 +0100

    Add ts ignore

--- tribeify-web-frontend/src/utils/edenTreaty.ts
+++ tribeify-web-frontend/src/utils/edenTreaty.ts
@@ -1,4 +1,5 @@
 import type { App } from "../../../tribeify-backend/app/src/index";
 import { treaty } from "@elysiajs/eden";
 
+//@ts-ignore
 export const backendApi = treaty<App>("localhost:8080");
